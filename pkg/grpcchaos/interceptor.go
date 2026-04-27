5644d2ac803a48bc580e01b6fa6a44fc15c1ade4
Cem Akan <cemakan5014@gmail.com>
2026-04-27 21:28:49 +0300

    feat(grpc): setup basic unary interceptor skeleton

--- pkg/grpcchaos/interceptor.go
+++ pkg/grpcchaos/interceptor.go
@@ -1,1 +1,20 @@
 package grpcchaos
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+
+	"github.com/CemAkan/pastaay/pkg/config"
+)
+
+// UnaryInterceptor returns a gRPC server interceptor that applies chaos policies.
+func UnaryInterceptor(cfgManager *config.Manager) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+
+		// TODO: Chaos logic will be implemented here
+
+		// Proceed to the actual RPC handler normally
+		return handler(ctx, req)
+	}
+}
