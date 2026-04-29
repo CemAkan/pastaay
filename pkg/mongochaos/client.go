efe79111b516367e19e21cb02d79f18344858654
Cem Akan <cemakan5014@gmail.com>
2026-04-29 16:28:42 +0300

    feat(mongo): add utility to apply chaos to client options

--- pkg/mongochaos/client.go
+++ pkg/mongochaos/client.go
@@ -1,1 +1,24 @@
 package mongochaos
+
+import (
+	"net"
+
+	"github.com/CemAkan/pastaay/pkg/config"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+// ApplyChaos integrates Pastaay fault injection into MongoDB ClientOptions.
+func ApplyChaos(opts *options.ClientOptions, mgr *config.Manager) *options.ClientOptions {
+	if opts == nil {
+		opts = options.Client()
+	}
+
+	// Inject both the monitor and the dialer into the client configuration.
+	opts.SetMonitor(NewChaosMonitor(mgr))
+	opts.SetDialer(&ChaosDialer{
+		DefaultDialer: &net.Dialer{},
+		Manager:       mgr,
+	})
+
+	return opts
+}
